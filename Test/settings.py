0c593e1c1b231d51d4e9c9855d9794230817d985
Lang-Chi Yu <ockhamsrazor@YLC-GT680.(none)>
2014-03-04 11:13:41 +0800

    add base.html

--- Test/settings.py
+++ Test/settings.py
@@ -114,6 +114,6 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    '/home/ockhamsrazor/Desktop/',
+    PROJECT_ROOT + '/../templates/',
     PROJECT_ROOT + '/../clock_skew/templates/',
 )
